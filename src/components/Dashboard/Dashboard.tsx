ca44d226df3bfe540b1e97609431bea808bc0ede
Dylan Ludwick <dylan@devhominid.com>
2021-11-28 12:28:15 -0700

    add DashboardLink component

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,7 +3,9 @@ import { useAppSelector } from '../../app/hooks';
 import { selectActiveService } from '../../slices/services';
 import { calculateRoutes } from './utils';
 import Button from '../Button';
+import DashboardLink from './DashboardLink';
 import Menu from '../Menu';
+import { ReactComponent as UFOIcon } from '../../assets/img/ufo.svg';
 import styles from './Dashboard.module.css';
 
 function Dashboard() {
@@ -37,18 +39,39 @@ function Dashboard() {
                   <span>{}</span>
                 </li>
                 <li>
-                  <span>Routers:</span>
-                  <span>{activeService.routers.length}</span>
+                  <span>Login:</span>
+                  <span className={activeService.login.enabled ? styles.enabledText : styles.disabledText}>
+                    {activeService.login.enabled ? 'Enabled' : 'Disabled'}
+                  </span>
                 </li>
                 <li>
-                  <span>Routes:</span>
-                  <span>{calculateRoutes(activeService)}</span>
+                  <span>Register:</span>
+                  <span className={activeService.register.enabled ? styles.enabledText : styles.disabledText}>
+                    {activeService.register.enabled ? 'Enabled' : 'Disabled'}
+                  </span>
                 </li>
               </ul>
+            </div>
+            <div className={styles.linkWrapper}>
+              <DashboardLink href="/routers" icon={['far', 'hdd']} text="Routers" />
+              <DashboardLink href="/databases" icon={['fas', 'database']} text="DBs" />
+              <DashboardLink href="/security" icon={['fas', 'lock']} text="Security" />
+            </div>
+            <div className={styles.buttonWrapper}>
+              <Button
+                clickHandler={() => console.log('clicked')}
+                text="Generate Service"
+              />
+              <Button
+                clickHandler={() => console.log('clicked')}
+                color="#1F1F1F"
+                text="Back"
+              />
             </div>
           </div>
         ) : (
           <div className={styles.noServiceSelected}>
+            <UFOIcon />
             <h1>Select a Service or Create a New One to Begin…</h1>
             <Button
               clickHandler={() => console.log('clicked')}
