55d418b43d6b70c4601281d3d12456c4954a90f9
Dylan Ludwick <dylan@devhominid.com>
2021-11-28 12:50:35 -0700

    add landing page and footer component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import Dashboard from '../Dashboard';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHdd } from '@fortawesome/free-regular-svg-icons';
+import { faDatabase, faLock, faToolbox } from '@fortawesome/free-solid-svg-icons';
+// import Dashboard from '../Dashboard';
+import Footer from '../Footer';
 import Header from '../Header';
+import Landing from '../Landing';
 import styles from './App.module.css';
+
+library.add(faDatabase, faHdd, faLock, faToolbox);
+console.dir(library);
 
 function App() {
   return (
-    <div className={styles.App}>
-      <Header />
-      <div className={styles.container}>
-        <Dashboard />
+    <Router>
+      <div className={styles.App}>
+        <Switch>
+          {/* <Route path="/login">
+            <div>login</div>
+          </Route> */}
+          <Route path="/">
+            <>
+              <Landing />
+              <Footer />
+            </>
+          </Route>
+        </Switch>
+        {/* <Header /> */}
+        {/* <div className={styles.container}>
+          <Dashboard />
+          
+        </div> */}
       </div>
-    </div>
+    </Router>
   );
 }
 
